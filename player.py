5c2e35ebc62910c00827208c2b72164154db2ad6
GrizzledLotus <prpolchinski@gmail.com>
2024-12-13 11:16:26 -0500

    J: 3-4: Shooting Rate

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         # Add any additional initialization for the Player class here
         self.rotation = 0
+        self.shoot_timer = 0
         print(f"Player groups after init: {self.groups()}")
 
     
@@ -33,10 +34,15 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def update(self, dt):
+        self.shoot_timer -= dt
+        
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
